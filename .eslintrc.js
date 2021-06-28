b0f58e7ce31f90670ee0b3a793b2b7636d8ed44e
Udit Karode <udit.karode@gmail.com>
2021-06-28 20:53:11 +0530

    [feat:root] add eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'prettier/prettier': 0,
+    quotes: 0,
+    semi: 0,
+    'keyword-spacing': 0,
+    curly: 0,
+    'comma-dangle': 0,
+    'no-trailing-spaces': 0,
+    'react-native/no-inline-styles': 0,
+    'react-hooks/exhaustive-deps': 0,
+    'eslint-comments/no-unused-disable': 0,
+  },
 };
