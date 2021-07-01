64ed8857f1c85871ddd36930e12f116d5f4b683b
Udit Karode <udit.karode@gmail.com>
2021-07-01 19:05:24 +0530

    [feat:screens/Login] animate login field

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react';
 
 import {StackScreenProps} from '@react-navigation/stack';
 import {ThemeContext} from '../utils/ThemeContext';
-import {ImageBackground, View} from 'react-native';
+import {ImageBackground, View, KeyboardEvent, Keyboard} from 'react-native';
 import {IconButton} from 'react-native-paper';
 import {StyleSheet} from 'react-native';
 import {getCommonStyles} from '../utils/CommonStyles';
@@ -13,20 +13,48 @@ import LoginIcon from '../../assets/login_page/login_icon.svg';
 import changeNavigationBarColor from 'react-native-navigation-bar-color';
 import ForgotIcon from '../../assets/login_page/forgot_icon.svg';
 import {Lock, User} from 'react-native-feather';
+import {animated, useSpring} from '@react-spring/native';
 const LoginBackground = require('../../assets/login_page/login_bg.png');
 
 type Props = StackScreenProps<StackParamList, 'Login'>;
 
 export default function Login(_navProps: Props) {
   let {theme} = React.useContext(ThemeContext);
+
   const styles = getCommonStyles(theme);
 
+  const _kbShown = (e: KeyboardEvent) =>
+    inputPropsApi.start({
+      from: {marginBottom: 0},
+      to: {marginBottom: e.endCoordinates.height / 1.3},
+    });
+
+  const _kbHidden = () =>
+    inputPropsApi.start({
+      to: {marginBottom: 0},
+    });
+
+  const [inputProps, inputPropsApi] = useSpring(() => ({
+    marginBottom: 0,
+  }));
+
   // effects
+  useEffect(() => {
+    // set up keyboard related listeners
+    Keyboard.addListener('keyboardDidShow', _kbShown);
+    Keyboard.addListener('keyboardDidHide', _kbHidden);
+
+    return () => {
+      Keyboard.removeAllListeners('keyboardDidShow');
+      Keyboard.removeAllListeners('keyboardDidHide');
+    };
+  }, []);
+
   useEffect(() => {
     setTimeout(() => {
       changeNavigationBarColor('#191916', true, false);
     }, 100);
-  }, []);
+  }, [theme]);
 
   // local styles
   const ls = StyleSheet.create({
@@ -41,7 +69,7 @@ export default function Login(_navProps: Props) {
       width: 60,
       borderRadius: 60,
       backgroundColor: theme.accentColor + '60',
-      marginHorizontal: 16,
+      marginHorizontal: 20,
     },
     miscButton: {
       height: 50,
@@ -58,16 +86,16 @@ export default function Login(_navProps: Props) {
           <AnibleHeader fill={theme.text} style={{marginTop: 60}} />
 
           {/* Main Login Content */}
-          <View style={{width: '100%'}}>
+          <animated.View style={{width: '100%', ...inputProps}}>
             <IconTextInput icon={User} hint="Username or Email" theme={theme} style={{alignSelf: 'center'}} />
             <IconTextInput icon={Lock} hint="Password" theme={theme} style={{marginTop: 15, alignSelf: 'center'}} />
 
-            <View style={{paddingHorizontal: 20, paddingVertical: 15, alignSelf: 'center', flexDirection: 'row', alignItems: 'center'}}>
+            <View style={{paddingHorizontal: 20, paddingVertical: 20, alignSelf: 'center', flexDirection: 'row', alignItems: 'center'}}>
               <IconButton icon={GoogleIcon} style={{...ls.miscButton, backgroundColor: '#fbfbfb', opacity: 0.8}} />
               <IconButton icon={LoginIcon} style={ls.loginButton} />
               <IconButton icon={ForgotIcon} style={{...ls.miscButton, backgroundColor: '#fbfbfb', opacity: 0.8}} />
             </View>
-          </View>
+          </animated.View>
         </View>
       </ImageBackground>
     </View>
