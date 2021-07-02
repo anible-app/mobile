77988347e671419ac1f12eea27dcab9653f819d1
Udit Karode <udit.karode@gmail.com>
2021-07-02 17:02:46 +0530

    [feat:screens] add Home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 import {StackScreenProps} from '@react-navigation/stack';
 import {ThemeContext} from '../utils/ThemeContext';
-
-import {View} from 'react-native';
+import AnibleHeader from '../../assets/anible_header.svg';
+import {StyleSheet, View} from 'react-native';
 import {getCommonStyles} from '../utils/CommonStyles';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {Settings} from 'react-native-feather';
+import {Text} from 'react-native';
 
 type Props = StackScreenProps<StackParamList, 'Home'>;
 
@@ -13,10 +16,34 @@ export default function Home(_navProps: Props) {
 
   const styles = getCommonStyles(theme);
 
+  const ls = StyleSheet.create({
+    headerView: {
+      paddingVertical: 20,
+      paddingHorizontal: 25,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    bodyView: {
+      paddingVertical: 10,
+      paddingHorizontal: 25,
+    },
+  });
+
   return (
-    <View style={styles.root}>
-      {/* debug square, remove this! */}
-      <View style={{height: 200, width: 200, backgroundColor: '#f44336'}} />
-    </View>
+    <SafeAreaView style={styles.safe}>
+      <View style={styles.root}>
+        {/* Header */}
+        <View style={ls.headerView}>
+          <AnibleHeader fill={theme.text} />
+          <Settings stroke={theme.text} style={{marginTop: 12}} />
+        </View>
+
+        {/* Body */}
+        <View style={ls.bodyView}>
+          <Text style={{...styles.textSemiBold, fontSize: 22}}>Currently trending</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
